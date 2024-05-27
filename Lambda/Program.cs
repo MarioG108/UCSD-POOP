7c43897cdc370a2804e77c40869f7ad3a1220c08
MarioG108 <44847760+MarioG108@users.noreply.github.com>
2024-05-27 15:57:18 -0400

    comments

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -5,7 +5,7 @@ public class Program
 
     public static void Main()
     {
-        // Lista de datos
+        // Lista de datos de la familia Perez
         List<Person> people = new List<Person>
         {
             new Person("John","Perez" , 30),
@@ -15,11 +15,15 @@ public class Program
             new Person("Joe","Perez" , 22 )
         };
         const int AdultAge = 18;
-        List<Person> adults = people.Where(x => x.Age >= AdultAge).ToList<Person>();
-        List<Person> children = people.Where(x => x.Age < AdultAge).ToList<Person>();
 
+        //filtra las personas mientras su edad sea mayor que la variable AdultAge, osea mayor de edad
+        List<Person> adults = people.Where(x => x.Age >= AdultAge).ToList<Person>();
+        //filtra las personas mientras su edad sea menor que la variable AdultAge, osea menor de edad
+        List<Person> children = people.Where(x => x.Age < AdultAge).ToList<Person>();
+        //escribe el nombre de todos los adultos
         adults.ForEach(t => { Console.WriteLine($"My Name is: {t.FullName} and I'm an adult, my age is {t.Age} years old"); });
         Console.WriteLine();
+        //escribe el nombre de todos los menores de edad
         children.ForEach(t => { Console.WriteLine($"My Name is: {t.FullName} and I'm a children, my age is {t.Age} years old"); });
 
         var oldestOne = adults.FirstOrDefault(x => x.Age == adults.Max(t => t.Age));
@@ -38,13 +42,21 @@ public class Program
         private string _firstName { get; set; }
         private string _lastName { get; set; }
         public int Age { get; set; }
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="lastname"></param>
+        /// <param name="age"></param>
         public Person(string name, string lastname, int age)
         {
             _firstName = name;
             _lastName = lastname;
             Age = age;
         }
+        /// <summary>
+        /// Retorna el nombre completo de la persona
+        /// </summary>
         public string FullName => _firstName + " " + _lastName;
 
     }
