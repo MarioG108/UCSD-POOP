6dd1591382a3fa8a11cdbc407407000f9d8a42b1
Mario Alberto Gonzalez Aquino <mar.gonzalez@tc.gob.do>
2024-06-04 09:37:01 -0400

    calculadora de consola HW

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿public static class Calculadora
+{
+
+    public static double GetDivision(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("No se puede dividir por cero.");
+            return double.NaN; // Not a Number
+        }
+        else
+        {
+            return num1 / num2;
+        }
+    }
+
+    public static double GetMultiplicacion(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+    public static double GetNumericDobleInput()
+    {
+        double input;
+        while (!double.TryParse(Console.ReadLine(), out input))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Por favor, ingrese un valor numérico válido.");
+            Console.ResetColor();
+        }
+        return input;
+    }
+
+    public static int GetNumericIntInput()
+    {
+
+        int input;
+        while (!int.TryParse(Console.ReadLine(), out input))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Por favor, ingrese un valor numérico válido.");
+            Console.ResetColor();
+        }
+        return input;
+    }
+
+    public static double GetResta(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+    public static double GetSuma(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+}
