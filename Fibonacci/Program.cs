cc4d85a5dccf718a50b2f0e73d70b3ca4ac3bb03
MarioG108 <44847760+MarioG108@users.noreply.github.com>
2024-06-10 08:29:43 -0400

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la cantidad de números de la secuencia Fibonacci que desea generar:");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Secuencia Fibonacci:");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Precione una tecla para cerrar");
+        Console.ReadKey();
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
